b8f91a6f35b01d4dc69724d025ac437042df4217
Shehan Chalaka Fernando <shehanchalakafdo@gmail.com>
2020-10-31 20:58:59 +0800

    updates

---
+++ device.py
@@ -0,0 +1,14 @@
+import time
+import board
+import adafruit_ina260
+ 
+class Device:
+    """
+    Battery Monitor Library
+    """
+    def __init__(self):
+        self.i2c = board.I2C()
+        self.ina260 = adafruit_ina260.INA260(self.i2c)
+
+    def readVoltage():
+        return self.ina260.voltage
