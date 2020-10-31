aa6f37fd7418f9763cc9489e9b4a0e335e1a4234
Shehan Chalaka Fernando <shehanchalakafdo@gmail.com>
2020-10-31 21:21:50 +0800

    updates

--- battery_monitor.py
+++ battery_monitor.py
@@ -2,6 +2,7 @@ import time
 import board
 import adafruit_ina260
 import adafruit_si7021
+import adafruit_ds3502
 
 class Device:
     """
@@ -11,6 +12,7 @@ class Device:
         self.i2c = board.I2C()
         self.ina260 = adafruit_ina260.INA260(self.i2c)
         self.si7021 = adafruit_si7021.SI7021(self.i2c)
+        self.ds3502 = adafruit_ds3502.DS3502(self.i2c)
 
         print("Device initialized")
 
@@ -22,6 +24,10 @@ class Device:
 
     def readPower(self):
         return self.ina260.power
+
+    def setVoltage(self, _voltage):
+        _wiper = _voltage
+        self.ds3502.wiper = _wiper
 
     def readTemperature(self):
         return self.si7021.temperature
