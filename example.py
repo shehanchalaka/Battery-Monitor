1f9277100c2f42f885b9c6b2c5eb052dda94c02f
Shehan Chalaka Fernando <shehanchalakafdo@gmail.com>
2020-11-01 01:47:06 +0800

    updates

--- example.py
+++ example.py
@@ -3,19 +3,15 @@ import battery_monitor
 
 device = battery_monitor.Device()
 
-device.setVoltage(4.0)
-
 while True:
     voltage = device.readVoltage()
     current = device.readCurrent()
     power = device.readPower()
     temperature = device.readTemperature()
     humidity = device.readHumidity()
+    device.setVoltage(4.0)
 
-    text = "V {:.3f} C {:.3f} Temperature = {:.2f} Humidity = {:.2f}\n".format(voltage, current, temperature, humidity)
-    
+    text = "Voltage = {:.2f} Current = {:.2f}\n".format(voltage, current)
     print(text)
-
     device.appendFile("./test.txt", text)
-
     time.sleep(1)
--- ina260_simpletest.py
+++ ina260_simpletest.py
@@ -3,7 +3,7 @@ import board
 import adafruit_ina260
  
 i2c = board.I2C()
-ina260 = adafruit_ina260.INA260(i2c)
+ina260 = adafruit_ina260.INA260(i2c, 0x41)
 while True:
     print(
         "Current: %.2f mA Voltage: %.2f V Power:%.2f mW"
