3290d5d5f1e79a1f1f892d820e4de963d2970ecf
Shehan Chalaka Fernando <shehanchalakafdo@gmail.com>
2020-11-01 00:56:49 +0800

    updates

--- example.py
+++ example.py
@@ -15,6 +15,6 @@ while True:
         % (voltage, current, power, temperature, humidity)
     )
 
-    device.appendFile("./test.txt", "Temperature = {} Humidity = {}".format(voltage, current))
+    device.appendFile("./test.txt", "Temperature = {} Humidity = {}\n".format(voltage, current))
 
-    time.sleep(1)
+    time.sleep(0.1)
