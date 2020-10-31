f3985cea6ec6bdfb297b14b9ddb67c8c2ec67b61
Shehan Chalaka Fernando <shehanchalakafdo@gmail.com>
2020-10-31 21:01:30 +0800

    updates

---
+++ example.py
@@ -0,0 +1,10 @@
+import battery_monitor
+
+device = battery_monitor.Device()
+
+while True:
+    voltage = device.readVoltage()
+
+    print("Voltage: %.2f V" % ina260.voltage)
+    
+    time.sleep(1)
