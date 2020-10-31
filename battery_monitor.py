f6c6ac408b2bdcb0d6cefb90004879baed4157ab
Shehan Chalaka Fernando <shehanchalakafdo@gmail.com>
2020-10-31 21:15:01 +0800

    updates

--- battery_monitor.py
+++ battery_monitor.py
@@ -1,7 +1,8 @@
 import time
 import board
 import adafruit_ina260
- 
+import adafruit_si7021
+
 class Device:
     """
     Battery Monitor Library
@@ -9,8 +10,21 @@ class Device:
     def __init__(self):
         self.i2c = board.I2C()
         self.ina260 = adafruit_ina260.INA260(self.i2c)
+        self.si7021 = adafruit_si7021.SI7021(self.i2c)
 
         print("Device initialized")
 
     def readVoltage(self):
         return self.ina260.voltage
+
+    def readCurrent(self):
+        return self.ina260.current
+
+    def readPower(self):
+        return self.ina260.power
+
+    def readTemperature(self):
+        return self.si7021.temperature
+
+    def readHumidity(self):
+        return self.si7021.relative_humidity
--- example.py
+++ example.py
@@ -5,7 +5,14 @@ device = battery_monitor.Device()
 
 while True:
     voltage = device.readVoltage()
+    current = device.readCurrent()
+    power = device.readPower()
+    temperature = device.readTemperature()
+    humidity = device.readHumidity()
 
-    print("Voltage: %.2f V" % voltage)
+    print(
+        "Current: %.2f mA Voltage: %.2f V Power:%.2f mW\nTemperature: %0.1f C Humidity: %0.1f %%"
+        % (voltage, current, power, temperature, humidity)
+    )
 
     time.sleep(1)
